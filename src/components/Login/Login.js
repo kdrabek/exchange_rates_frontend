6f9855309ba3efe5b317b9d6ea6f51fed861dab5
Krzysztof Drabek <kdrabek@gmail.com>
2017-03-04 15:28:42 +0100

    Add basic Login and Register components.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Link } from 'react-router';
+import { Row, Col, FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        Hi from Login component!
-        <Button bsStyle="success">Login component</Button>
-      </div>
+      <Row>
+        <Col md={6} mdOffset={3}>
+          <h4>Logowanie</h4>
+          <form>
+            <FormGroup>
+              <ControlLabel htmlFor="email">Email: </ControlLabel>
+              <FormControl id="email"/>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel htmlFor="password">Hasło: </ControlLabel>
+              <FormControl id="password"/>
+            </FormGroup>
+            <FormGroup>
+              <Button bsStyle="primary" type="submit" block>Zaloguj się</Button>
+            </FormGroup>
+          </form>
+          <Link to="register">Nie masz konta? Zarejestruj się.</Link>
+        </Col>
+      </Row>
     );
   }
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Link } from 'react-router';
+import { Row, Col, FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
 
 class Register extends Component {
   render() {
     return (
-      <div>
-        Hi from Register component!
-        <Button bsStyle="success">Register component</Button>
-      </div>
+      <Row>
+        <Col md={6} mdOffset={3}>
+          <h4>Rejestracja</h4>
+          <form>
+            <FormGroup>
+              <ControlLabel htmlFor="email">Email: </ControlLabel>
+              <FormControl id="email"/>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel htmlFor="password">Hasło: </ControlLabel>
+              <FormControl id="password"/>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel htmlFor="password2">Powtórz hasło: </ControlLabel>
+              <FormControl id="password2"/>
+            </FormGroup>
+            <FormGroup>
+              <Button bsStyle="primary" type="submit" block>Zarejestruj się</Button>
+            </FormGroup>
+          </form>
+          <Link to="login">Masz już konto? Zaloguj się</Link>
+        </Col>
+      </Row>
     );
   }
 }
