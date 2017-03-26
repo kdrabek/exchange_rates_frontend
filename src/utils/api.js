9685ff97533ae79677b6185d9355d58f1ba5b035
Krzysztof Drabek <kdrabek@gmail.com>
2017-03-26 21:00:35 +0200

    Refactor api.

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,148 +3,79 @@ const BASE_API_URL = 'http://localhost';
 class Api {
   
   getCurrentRates() {
-    const url = '/rates/rates';
-    return fetch(`${BASE_API_URL}${url}`).then(response => {
-      return response.json();
-    }).catch(err => {return err;});
+    return this._fetch('GET', '/rates/rates');
   }
 
   getRatesForDate(date) {
-    const url = `/rates/rates/${date}`;
-    return fetch(`${BASE_API_URL}${url}`).then(response => {
-      return response.json();
-    }).catch(err => {return err;});
+    return this._fetch('GET', `/rates/rates/${date}`);
   }
 
   getRatesForCurrency(token, currency, days) {
     const url = `/rates/rates/${currency}/limit/${days}`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-
-    return fetch(
-      `${BASE_API_URL}${url}`,
-      {
-        method: 'GET',
-        headers: headers,
-        mode: 'cors'
-      }
-    ).then(response => {
-      return response.json();
-    }).catch(err => {return err;});
+    return this._fetch('GET', url, token);
   }
 
   getNotifications(token) {
     const url = `/notifications/${token}`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-
-    return fetch(
-      `${BASE_API_URL}${url}`, {method: 'GET', headers: headers, mode: 'cors'}
-    ).then(response => { 
-      return response.json(); 
-    }).catch(err => {return err;});
+    return this._fetch('GET', url, token);
   }
   
   deleteNotification(token, notificationId) {
     const url = `/notifications/${token}/${notificationId}`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-
-    return fetch(
-      `${BASE_API_URL}${url}`,
-      {
-        method: 'DELETE',
-        headers: headers,
-        mode: 'cors'
-      }
-    ).then(response => {
-      return {id: notificationId};
-    }).catch(err => {return err;});
+    return this._fetch('DELETE', url, token, notificationId).then(
+      response => { return {id: notificationId};}
+    );
   }
 
   updateNotification(token, notification) {
     const url = `/notifications/${token}/${notification.id}`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-    return fetch(
-      `${BASE_API_URL}${url}`,
-      {
-        method: 'PUT',
-        headers: headers,
-        mode: 'cors',
-        body: JSON.stringify(notification)
-      }
-    ).then(
-      response => response.json()
-    ).catch(
-      err => {return err;}
-    );
+    return this._fetch('PUT', url, token, notification);
   }
   
   addNotification(token, notification) {
     const url = `/notifications/${token}`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-    return fetch(
-      `${BASE_API_URL}${url}`,
-      {
-        method: 'POST',
-        headers: headers,
-        mode: 'cors',
-        body: JSON.stringify(notification)
-      }
-    ).then(response => response.json()
-    ).then(response => {
-      return {id: response.id, ...notification};
-    }).catch(err => {return err;});
+    return this._fetch('POST', url, token, notification).then(
+      response => { return {id: response.id, ...notification};}
+    );
   }
 
   getCurrencies(token) {
     const url = `/rates/currency`;
-    let headers = new Headers();
-    headers.append('Authorization', `Token ${token}`);
-
-    return fetch(
-      `${BASE_API_URL}${url}`,
-      {
-        method: 'GET',
-        headers: headers,
-        mode: 'cors'
-      }
-    ).then(response => {
-      return response.json();
-    }).catch(err => {return err;});
+    return this._fetch('GET', url, token);
   }
 
   login(user) {
     const url = '/auth/login';
-    return fetch(`${BASE_API_URL}${url}`, {
-      method: 'POST',
-      body: JSON.stringify(user)
-    })
-    .then(response => {
-      return response.json().then(json => {
-      if (!response.ok)
-        return Promise.reject(json);
-      return json;
-      })
-    })
+    return this._fetch('POST', url, null, user);
   }
   
   register(user) {
     const url = '/auth/register';
-    return fetch(`${BASE_API_URL}${url}`, {
-      method: 'POST',
-      body: JSON.stringify(user)
-    })
-    .then(response => {
-      return response.json().then(json => {
-      if (!response.ok)
-        return Promise.reject(json);
-      return json;
-      })
-    })
+    return this._fetch('POST', url, null, user);
   }
+
+  _prepareHeaders(token){
+    let headers = new Headers();
+    headers.append('Authorization', `Token ${token}`);
+    return headers;
+  }
+
+  _fetch(method, url, token=null, body=null){
+    let request = {method: method, mode: 'cors'}
+    if (token)
+      request.headers = this._prepareHeaders(token)
+    if (body)
+      request.body = JSON.stringify(body)
+  
+    return fetch(
+      `${BASE_API_URL}${url}`, request
+      ).then(
+        response => response.json()
+      ).catch(
+        err => {return err;}
+      );
+  }
+
 }
 
 export default Api;
