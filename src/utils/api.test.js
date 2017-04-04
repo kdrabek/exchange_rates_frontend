00f290f679944ff6e625a18ec339152931005ca1
Krzysztof Drabek <kdrabek@gmail.com>
2017-04-04 20:52:13 +0200

    Moar api tests.

--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -73,21 +73,21 @@ describe('Rates Endpoint', () => {
   });
 
   it('(getRatesForCurrency) should use proper request params', (done) => {
-    
-    return ratesApi.getRatesForCurrency('token', 'USD', '1').then(resp => {
+    const token = 'token';
+    return ratesApi.getRatesForCurrency(token, 'USD', '1').then(resp => {
       const mockCallParams = fetchMock.lastCall()
       expect(mockCallParams[1]).toEqual({
         method: "GET", 
         mode: "cors",
-        headers: ratesApi._prepareHeaders('token')
+        headers: ratesApi._prepareHeaders(token)
       });
       done();
     });
   });
 
   it('(getRatesForCurrency) should use return data in correct format', (done) => {
-    
-    return ratesApi.getRatesForCurrency('token', 'USD', '1').then(resp => {
+    const token = 'token';
+    return ratesApi.getRatesForCurrency(token, 'USD', '1').then(resp => {
       expect(resp).toHaveProperty('limit');
       expect(resp).toHaveProperty('rates');
 
@@ -95,6 +95,83 @@ describe('Rates Endpoint', () => {
       done();
     });
 
+  });
+
+})
+
+
+describe('Currencies Endpoint', () => {
+
+  const token = 'token';
+
+  beforeAll(()=>{
+    const expectedResp = {
+      currencies: [{
+        code: "THB",
+        name: "bat (Tajlandia)",
+        country: "Tajlandia",
+        table_type: "A"
+      }]
+    };
+    fetchMock.get(`${BASE_API_URL}/rates/currency`, expectedResp);
+  });
+
+  it('(getCurrencies) should use proper request params', (done) => {
+    
+    return ratesApi.getCurrencies(token).then(resp => {
+      const mockCallParams = fetchMock.lastCall()
+      
+      expect(mockCallParams[1]).toEqual({
+        method: 'GET', mode: 'cors', headers: ratesApi._prepareHeaders(token)
+      });
+      done();
+    });
+  });
+
+  it('(getCurrencies) should use return data in correct format', (done) => {
+    
+    return ratesApi.getCurrencies(token).then(resp => {
+      expect(resp).toHaveProperty('currencies');
+      expect(resp.currencies.length).toBe(1);
+      done();
+    });
+
+  });
+
+})
+
+
+describe('Auth Endpoint', () => {
+
+  const testUser = {email: 'test@gmail.com', password: 'password'};
+
+  beforeAll(()=>{
+    const expectedResp = {id: 1, token: 'token'};
+    fetchMock.post(`${BASE_API_URL}/auth/login`, expectedResp);
+    fetchMock.post(`${BASE_API_URL}/auth/register`, expectedResp);
+  });
+
+  it('(login) should use proper request params', (done) => {
+    
+    return ratesApi.login(testUser).then(resp => {
+      const mockCallParams = fetchMock.lastCall()
+      expect(mockCallParams[1]).toEqual({
+        method: 'POST', mode: 'cors', body: JSON.stringify(testUser) 
+      });
+      done();
+    });
+  });
+
+  it('(register) should use proper request params', (done) => {
+    
+    return ratesApi.register(testUser).then(resp => {
+      const mockCallParams = fetchMock.lastCall()
+      
+      expect(mockCallParams[1]).toEqual({
+        method: 'POST', mode: 'cors', body: JSON.stringify(testUser) 
+      });
+      done();
+    });
   });
 
 })
